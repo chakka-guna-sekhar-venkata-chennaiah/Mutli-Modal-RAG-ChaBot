c37ee2d62f1841656e78163ca95ef8f44d16f237
chakka-guna-sekhar-venkata-chennaiah <sekharchennaiah12345ctk@gmail.com>
2024-09-16 19:32:13 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ from langchain.prompts import PromptTemplate
 from langchain.embeddings.base import Embeddings
 from langchain.llms.base import LLM
 from pydantic import BaseModel, Field
+import streamlit as st
 
 
 client = OpenAI(
